b99fa6c91895144e273fd006edf87604e62f9ce0
oriontian <orionwtian@gmail.com>
2024-07-26 14:11:00 -0500

    fixed edit-climb page

--- client/src/pages/edit-climb.js
+++ client/src/pages/edit-climb.js
@@ -1,144 +1,114 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
+import { useParams } from 'react-router-dom';
 import axios from 'axios';
 
-export default class EditClimb extends Component {
-  constructor(props) {
-    super(props);
+const EditClimb = () => {
+  const { id } = useParams();
+  const [username, setUsername] = useState('');
+  const [description, setDescription] = useState('');
+  const [grade, setGrade] = useState(0);
+  const [date, setDate] = useState(new Date());
+  const [users, setUsers] = useState([]);
+  const [error, setError] = useState('');
 
-    this.onChangeUsername = this.onChangeUsername.bind(this);
-    this.onChangeDescrip = this.onChangeDescrip.bind(this);
-    this.onChangeGrade = this.onChangeGrade.bind(this);
-    this.onChangeDate = this.onChangeDate.bind(this);
-    this.onSubmit = this.onSubmit.bind(this);
-
-    this.state = {
-      username: '',
-      description: '',
-      grade: 0,
-      date: new Date(),
-      users: []
-    }
-  }
-
-  componentDidMount() {
-    axios.get('http://localhost:5000/climbs/' + this.props.match.params.id)
+  useEffect(() => {
+    axios.get('http://localhost:5000/climbs/' + id)
       .then(response => {
-        this.setState({
-          username: response.data.username,
-          description: response.data.description,
-          grade: response.data.grade,
-          date: new Date(response.data.date)
-        })
+        setUsername(response.data.username);
+        setDescription(response.data.description);
+        setGrade(response.data.grade);
+        setDate(new Date(response.data.date));
       })
-      .catch(function (error) {
-        console.log(error);
-      })
+      .catch(error => {
+        console.log('Error fetching climb data: ', error);
+        setError('Error fetching climb data');
+      });
 
     axios.get('http://localhost:5000/users/')
       .then(response => {
-        this.setState({ users: response.data.map(user => user.username) });
+        setUsers(response.data.map(user => user.username));
       })
-      .catch((error) => {
-        console.log(error);
-      })
-  }
+      .catch(error => {
+        console.log('Error fetching users: ', error);
+        setError('Error fetching users');
+      });
+  }, [id]);
 
-  onChangeUsername(e) {
-    this.setState({
-      username: e.target.value
-    });
-  }
-
-  onChangeDescrip(e) {
-    this.setState({
-      description: e.target.value
-    });
-  }
-
-  onChangeGrade(e) {
-    this.setState({
-      grade: e.target.value
-    });
-  }
-
-  onChangeDate(date) {
-    this.setState({
-      date: date
-    });
-  }
-
-  onSubmit(e) {
+  const onSubmit = (e) => {
     e.preventDefault();
 
     const climb = {
-      username: this.state.username,
-      description: this.state.description,
-      grade: this.state.grade,
-      date: this.state.date,
+      username,
+      description,
+      grade,
+      date,
     };
 
-    console.log(climb);
+    axios.post('http://localhost:5000/climbs/update/' + id, climb)
+      .then(res => {
+        console.log(res.data);
+        // navigate('/');
+      })
+      .catch(error => {
+        console.log('Error updating climb: ', error);
+        setError('Error updating climb');
+      });
 
-    axios.post('http://localhost:5000/climbs/update/' + this.props.match.params.id, climb)
-      .then(res => console.log(res.data));
 
     window.location = '/';
-  }
+  };
 
-  render() {
-    return (
-      <div>
-        <h3>Edit Climb Log</h3>
-        <form onSubmit={this.onSubmit}>
-          <div className="form-group">
-            <label>Username: </label>
-            <select ref="userInput"
-              className="form-control"
-              value={this.state.username}
-              onChange={this.onChangeUsername}>
-              {
-                this.state.users.map(function (user) {
-                  return <option
-                    key={user}
-                    value={user}>{user}
-                  </option>;
-                })
-              }
-            </select>
-          </div>
-          <div className="form-group">
-            <label>Description: </label>
-            <input type="text"
-              required
-              className="form-control"
-              value={this.state.description}
-              onChange={this.onChangeDescrip}
-            />
-          </div>
-          <div className="form-group">
-            <label>Grade (V-Scale): </label>
-            <input
-              type="text"
-              className="form-control"
-              value={this.state.duration}
-              onChange={this.onChangeDuration}
-            />
-          </div>
-          <div className="form-group">
-            <label>Date: </label>
-            <DatePicker
-              selected={this.state.date}
-              onChange={this.onChangeDate}
-            />
-          </div>
+  if (error) { return <div>{error}</div>; }
 
-          <div className="form-group">
-            <input type="submit" value="Edit Climb Log" className="btn btn-primary" />
-          </div>
-        </form>
-      </div>
-    )
-  }
+  return (
+    <div>
+      <h3>Edit Climb Log</h3>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label>Username: </label>
+          <select
+            className="form-control"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}>
+            {users.map(user => (
+              <option key={user} value={user}>{user}</option>
+            ))}
+          </select>
+        </div>
+        <div className="form-group">
+          <label>Description: </label>
+          <input type="text"
+            required
+            className="form-control"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label>Grade (V-Scale): </label>
+          <input
+            type="text"
+            className="form-control"
+            value={grade}
+            onChange={(e) => setGrade(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label>Date: </label>
+          <DatePicker
+            selected={date}
+            onChange={(e) => setDate(e.target.value)}
+          />
+        </div>
+
+        <div className="form-group">
+          <input type="submit" value="Edit Climb Log" className="btn btn-primary" />
+        </div>
+      </form>
+    </div>
+  )
 }
+
+export default EditClimb;
